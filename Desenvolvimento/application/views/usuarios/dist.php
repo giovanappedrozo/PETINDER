791d5c90e4a25706657bcbea116e502b0fa0da07
sp185089x <sp185089x@72fe1d9a-8248-0410-b756-cde062451638>
2021-10-06 18:10:38 +0000

    Excluindo arquivos errados
    git-svn-id: https://svn.spo.ifsp.edu.br/svn/a6pgp/A2021-PDS431/TI.TI.TI@71059 72fe1d9a-8248-0410-b756-cde062451638

--- Desenvolvimento/application/views/usuarios/dist.php
+++
@@ -1,8 +0,0 @@
-<?php foreach ($usuarios as $usuario): ?>
-        <main>
-                <?php
-                $ar = ["/[^0-9]/", "/[-]/", ];
-                $a = preg_replace($ar, "", $usuario['localizacao']);
-                echo $a; ?>
-        </main>
-<?php endforeach; ?>
